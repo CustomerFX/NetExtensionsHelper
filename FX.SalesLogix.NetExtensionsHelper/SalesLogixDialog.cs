fbb4d74ac773fcde31eb245e3ba32fe15615626c
Ryan Farley <ryan@farleyzone.com>
2012-11-30 00:46:55 -0700

    Added license info for clarity

--- FX.SalesLogix.NetExtensionsHelper/SalesLogixDialog.cs
+++ FX.SalesLogix.NetExtensionsHelper/SalesLogixDialog.cs
@@ -1,4 +1,40 @@
-﻿using System;
+﻿#region License Information
+/* 
+
+    SalesLogix Mobile Developer Tools
+    Copyright (C) 2012  Customer FX Corporation - http://customerfx.com/
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	
+    ADDITIONAL TERMS: 
+    Attribution required.
+
+    Contact Information:
+    
+    Ryan Farley 
+    Customer FX Corporation
+    http://customerfx.com/
+    2324 University Avenue West, Suite 115
+    Saint Paul, Minnesota 55114
+    Tel: 651.646.7777  Fax: 651.846.5185
+    
+    This copyright must remain intact
+    
+*/
+#endregion
+
+using System;
 using System.Drawing;
 using System.Reflection;
 using System.Windows.Forms;
